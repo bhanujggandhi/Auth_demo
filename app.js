3eb117145148fa40f6759908c51fa52b9b6a5d4b
bhanujggandhi <gandhibhanuj@gmail.com>
2020-04-15 02:49:51 +0530

    Secret page added

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var express = require('express');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/auth_demo', { useNewUrlParser: true, useUnifiedTopology: true });
 
 var app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
 	res.render('home');
+});
+
+app.get('/secret', function(req, res) {
+	res.render('secret');
 });
 
 app.listen(3000, function() {
